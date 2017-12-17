88232cf16c8b6fea34f22d699c56797bd8cf0264
Wojciech Maj <kontakt@wojtekmaj.pl>
2017-12-17 23:01:19 +0100

    Implement better time prop checking

--- src/shared/propTypes.js
+++ src/shared/propTypes.js
@@ -1,10 +1,21 @@
 import PropTypes from 'prop-types';
 
-// eslint-disable-next-line import/prefer-default-export
-export const isTime = PropTypes.string; // TODO better check
-
 const allViews = ['hour', 'minute', 'second'];
+const hourOptionalSecondsRegExp = /^(([0-1])?[0-9]|2[0-3]):[0-5][0-9](:([0-5][0-9]))?$/;
+
+export const isTime = (props, propName, componentName) => {
+  const time = props[propName];
+
+  if (time) {
+    if (!hourOptionalSecondsRegExp.test(time)) {
+      return new Error(`Warning: Failed prop type: Invalid prop \`${propName}\` of type \`${typeof minDate}\` supplied to \`${componentName}\`, expected time in HH:mm(:ss) format.`);
+    }
+  }
+
+  // Everything is fine
+  return null;
+};
 
 export const isDetail = PropTypes.oneOf(allViews);
 
-export const isValueType = PropTypes.oneOf(allViews);
+export const isValueType = isDetail;
