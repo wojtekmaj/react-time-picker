d0a3b396c8d2a38c9ab0bd5b7041334cabbd5e2a
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-01-05 01:10:09 +0100

    Fix fieldset legend

--- test/ValueOptions.jsx
+++ test/ValueOptions.jsx
@@ -14,8 +14,8 @@ export default class ValueOptions extends Component {
     const { value } = this.props;
 
     return (
-      <fieldset id="detailoptions">
-        <legend htmlFor="viewoptions">Set time externally</legend>
+      <fieldset id="valueOptions">
+        <legend htmlFor="valueOptions">Set time externally</legend>
 
         <div>
           <label htmlFor="time">Time</label>
