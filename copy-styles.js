91cfb6776d35e6e479ce577884bc128d006c365e
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-03-15 21:52:05 +0100

    Simplify building styles process

--- copy-styles.js
+++ copy-styles.js
@@ -7,11 +7,3 @@ fs.copyFile('src/TimePicker.less', 'dist/TimePicker.less', (error) => {
   // eslint-disable-next-line no-console
   console.log('TimePicker.less copied successfully.');
 });
-
-fs.copyFile('src/TimePicker.css', 'dist/TimePicker.css', (error) => {
-  if (error) {
-    throw error;
-  }
-  // eslint-disable-next-line no-console
-  console.log('TimePicker.css copied successfully.');
-});
