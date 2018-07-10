15b2fbd23e113be37a13e650d429937baf92639d
Wojciech Maj <kontakt@wojtekmaj.pl>
2018-07-10 09:48:10 +0200

    Fix import/no-unresolved error in entry.js

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,9 @@
 import 'react-clock/dist/Clock.css';
 
 import TimePicker from './TimePicker';
+
+// File is created during build phase and placed in dist directory
+// eslint-disable-next-line import/no-unresolved
 import './TimePicker.css';
 
 export default TimePicker;
