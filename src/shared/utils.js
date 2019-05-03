7ed3574de80ae4cd155e99360720f48fe95c3d76
Wojciech Maj <kontakt@wojtekmaj.pl>
2019-05-03 15:02:30 +0200

    Replace updateInputWidth with update-input-width util

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -5,20 +5,6 @@ const isValidNumber = a => typeof a === 'number' && !isNaN(a);
 export const min = (...args) => Math.min(...args.filter(isValidNumber));
 export const max = (...args) => Math.max(...args.filter(isValidNumber));
 
-export const updateInputWidth = (element) => {
-  const span = document.createElement('span');
-  span.innerHTML = element.value || element.placeholder;
-
-  const container = element.parentElement;
-
-  container.appendChild(span);
-
-  const { width } = span.getBoundingClientRect();
-  element.style.width = `${Math.ceil(width)}px`;
-
-  container.removeChild(span);
-};
-
 export const getAmPmLabels = (locale) => {
   const amPmFormatter = getFormatter({ hour: 'numeric' });
   const amString = amPmFormatter(locale, new Date(2017, 0, 1, 9));
