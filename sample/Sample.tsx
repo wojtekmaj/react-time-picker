8ad027acf75664c9e107c270461f52d54b03cf6f
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-09-22 12:39:57 +0200

    Use new JSX transform in all sample and test suites

--- sample/Sample.tsx
+++ sample/Sample.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import TimePicker from 'react-time-picker';
 
 import './Sample.css';
--- test/ValidityOptions.tsx
+++ test/ValidityOptions.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 type ValidityOptionsProps = {
   maxTime?: string;
   minTime?: string;
